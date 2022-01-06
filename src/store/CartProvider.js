3cc531083d73cef739a5f2eae24dc15731136e72
Haruna Waizumi <haruna.waizumi@gmail.com>
2022-01-06 13:55:34 +0900

    update carprovider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -28,9 +28,35 @@ const cartReducer = (state, action) => {
       };
 
       updatedItems = [...state.items];
-      updatedItems[existingCartItem] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  else if (action.type == 'REMOVE')
+  {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if(existingCartItem.amount == 1)
+    {
+      updatedItems = state.items.filter(item => item.id!== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
     }
 
     return {
